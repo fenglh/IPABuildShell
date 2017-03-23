5e1c865bb3ef570080508d10102b47da5369cf46
itx <itx@bluemoon007deiMac.local>
2017-03-23 10:40:02 +0800

    111

--- IPABuildShell.sh
+++ IPABuildShell.sh
@@ -441,9 +441,9 @@ function build
 		fi
 
 		if [[ $isExistXcWorkspace == true ]]; then
-			$xcodebuild archive -workspace $xcworkspace -scheme ${buildTargetNames[$i]} -archivePath $archivePath -configuration $configuration build DEVELOPMENT_TEAM=${newTeamId} CODE_SIGN_IDENTITY="${newCodeSign}" PROVISIONING_PROFILE=${newProfileUuid} 
+			$xcodebuild archive -workspace $xcworkspace -scheme ${buildTargetNames[$i]} -archivePath $archivePath -configuration $configuration build  
 		else
-			$xcodebuild archive						 	-scheme ${buildTargetNames[$i]} -archivePath $archivePath -configuration $configuration build DEVELOPMENT_TEAM=${newTeamId} CODE_SIGN_IDENTITY="${newCodeSign}" PROVISIONING_PROFILE=${newProfileUuid}
+			$xcodebuild archive						 	-scheme ${buildTargetNames[$i]} -archivePath $archivePath -configuration $configuration build 
 		fi
 		# $cmd
 		if [[ $? -ne 0 ]]; then
