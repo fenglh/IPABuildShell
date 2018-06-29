b2b3615e16e8966612cf46e40609af0cd1678d9c
itx <>
2018-06-29 14:28:02 +0800

    修复读取配置文件没有处理单引号的问题

--- IPABuildShell.sh
+++ IPABuildShell.sh
@@ -160,7 +160,7 @@ function getXcconfigValue() {
 		exit 1
 	fi
 	## 去掉//开头 ;  查找key=特征，去掉双引号
-	local value=$(grep -v "[ ]*//" "$xcconfigFile" | grep -e "[ ]*$key[ ]*=" | tail -1| cut -d "=" -f2 | grep -o "[^ ]\+\( \+[^ ]\+\)*" | sed 's/\"//g' ) 
+	local value=$(grep -v "[ ]*//" "$xcconfigFile" | grep -e "[ ]*$key[ ]*=" | tail -1| cut -d "=" -f2 | grep -o "[^ ]\+\( \+[^ ]\+\)*" | sed 's/\"//g' | sed "s/\'//g" ) 
 
 	echo $value
 }
